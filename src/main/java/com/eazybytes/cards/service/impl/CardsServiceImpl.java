58c12701951b5ea59542880c6b4648cb82ce61a9
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-12-26 09:05:58 +0200

    create service interface and its implementation

--- src/main/java/com/eazybytes/cards/service/impl/CardsServiceImpl.java
+++ src/main/java/com/eazybytes/cards/service/impl/CardsServiceImpl.java
@@ -1,4 +1,39 @@
 package com.eazybytes.cards.service.impl;
 
-public class CardsServiceImpl {
+import com.eazybytes.cards.dto.CardsDto;
+import com.eazybytes.cards.entity.Cards;
+import com.eazybytes.cards.repository.CardsRepository;
+import com.eazybytes.cards.service.ICardsService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class CardsServiceImpl implements ICardsService {
+
+    private CardsRepository cardsRepository;
+    @Override
+    public void createCard(String mobileNumber) {
+        Optional<Cards> optionalCards = cardsRepository.findByMobileNumber(mobileNumber);
+        if (optionalCards.isPresent()){
+
+        }
+    }
+
+    @Override
+    public CardsDto fetchCard(String mobileNumber) {
+        return null;
+    }
+
+    @Override
+    public boolean updateCard(CardsDto cardsDto) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteCard(String mobileNumber) {
+        return false;
+    }
 }
