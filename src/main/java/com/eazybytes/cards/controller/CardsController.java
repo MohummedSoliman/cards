3b7e2af37c75e7feaa3b7d73528faf591ca7aa4b
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-12-26 11:46:04 +0200

    create cards controller

---
+++ src/main/java/com/eazybytes/cards/controller/CardsController.java
@@ -0,0 +1,66 @@
+package com.eazybytes.cards.controller;
+
+import com.eazybytes.cards.constants.CardsConstants;
+import com.eazybytes.cards.dto.CardsDto;
+import com.eazybytes.cards.dto.ResponseDto;
+import com.eazybytes.cards.service.ICardsService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@AllArgsConstructor
+public class CardsController {
+
+    private ICardsService iCardsService;
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createCard(@RequestParam String mobileNumber) {
+        iCardsService.createCard(mobileNumber);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(
+                        CardsConstants.STATUS_201,
+                        CardsConstants.MESSAGE_201
+                ));
+    }
+
+    @GetMapping("/fetch")
+    public ResponseEntity<CardsDto> fetchCardDetails(@RequestParam String mobileNumber){
+        CardsDto cardsDto = iCardsService.fetchCard(mobileNumber);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(cardsDto);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDto> updateCardDetails(@RequestBody CardsDto cardsDto) {
+        boolean isUpdated = iCardsService.updateCard(cardsDto);
+        if(isUpdated) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(CardsConstants.STATUS_200, CardsConstants.MESSAGE_200));
+        }else{
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(CardsConstants.STATUS_417, CardsConstants.MESSAGE_417_UPDATE));
+        }
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<ResponseDto> deleteCardDetails(@RequestParam String mobileNumber) {
+        boolean isDeleted = iCardsService.deleteCard(mobileNumber);
+        if(isDeleted) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(CardsConstants.STATUS_200, CardsConstants.MESSAGE_200));
+        }else{
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(CardsConstants.STATUS_417, CardsConstants.MESSAGE_417_DELETE));
+        }
+    }
+}
